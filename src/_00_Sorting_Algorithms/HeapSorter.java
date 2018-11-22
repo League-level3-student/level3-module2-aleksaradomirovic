7854761659807dc23765d4331b83e7a34e30bffa
aleksaradomirovic <league@WTS22.attlocal.net>
2018-11-21 18:28:05 -0800

    heapsort

--- src/_00_Sorting_Algorithms/HeapSorter.java
+++ src/_00_Sorting_Algorithms/HeapSorter.java
@@ -5,59 +5,44 @@ public class HeapSorter extends Sorter {
 	public HeapSorter() {
 		type = "Heap";
 	}
-	
-	//1. Since this sorting algorithm uses recursion, 
-	//   we'll make a helper method called heapSort.
-	//   Complete the steps in the heapSort method.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		//8. create an integer called n and set it equal 
-		//   to the length of the array
-		int n = array.length;
-		//9. make a for loop that starts half way between
-		//   0 and n and counts down until it is less than 0.
-		//   Inside this for loop, call the heapSort method with n and i
-        for(int i = n/2; i >= 0; i--) {
-        		heapSort(array, n, i, display);
-        }
-        //10. make a for loop that starts at n-1 
-        //    and counts down until it is less than 0.
-        
-        	//11. swap the array elements at 0 and i.
- 
-            //12. call the heapSort method with i and 0
-        
-	}
-	
-	public void heapSort(int[] array, int n, int i, SortingVisualizer display) {
-		//2. create an integer called largest and set it equal to i
-		int largest = i;
-		//3. create an integer called l and set it equal to 2 * i + 1
-        int l = 2 * i + 1;
-		//4. create an integer called r and set it equal to 2 * i + 2
-        int r = 2 * i + 2;
-        //5. if l is less than n 
-        //   and array element at l is greater than array element at largest
-        //   then set largest equal to l
-        if(l < n && array[l] > array[largest]) {
-        		largest = l;
-        }
-        //6. if r is less than n 
-        //   and array element at r is greater than array element at largest
-        //   then set largest equal to r
-        if(r < n && array[r] > array[largest]) {
-        	largest = r;
-        }
-        //7. if largest is not equal to i
-        //   then swap the array elements at i and largest.
-        //   Also, call the heapSort method with n and largest
-        if(largest != i) {
-        		int temp = array[i];
-        		array[i] = array[largest];
-        		array[largest] = temp;
-        }
+		// int[] nArr = new int[array.length + 1];
+		//
+		// for (int i = 0; i < array.length; i++) {
+		// nArr[i] = array[i];
+		// }
+		//
+		// nArr[array.length] = SortingVisualizer.MAX;
+		//
+		// array = nArr;
+		while (!checkOrder(array)) {
+			for (int i = 0; i < array.length; i++) {
+				flip(array, i);
+			}
+			display.updateDisplay();
+		}
 	}
 
+	void flip(int[] array, int i) {
+		for (int j = 0; j < array.length; j++) {
+			if (array[j] > array[i] - 2 && j < i) {
+				int temp = array[j];
+				array[j] = array[i - 1];
+				array[i - 1] = temp;
+
+				return;
+			}
+		}
+	}
+
+	boolean checkOrder(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[i - 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
