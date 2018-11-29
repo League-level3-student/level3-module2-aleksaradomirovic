4c5f7dabd29e42d5f192e143540a40ce16fba089
hop324 <league@WTS22.attlocal.net>
2018-11-28 18:27:23 -0800

    finished sorting

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -6,20 +6,49 @@ public class BogoSorter extends Sorter {
 	public BogoSorter() {
 		type = "Bogo";
 	}
-	
-	// Bogo sort is a joke sorting algorithm. It is considered the most 
+
+	Random rnd = new Random();
+	final int updateInterval = 100000000;
+
+	// Bogo sort is a joke sorting algorithm. It is considered the most
 	// inefficient sorting algorithm while still maintaining the possibility
 	// of eventually sorting data.
-	
+
 	// It works by following these steps:
 	// STEP 1. Is the array in order?
-	//	if yes, finished; if no, go to step 2.
+	// if yes, finished; if no, go to step 2.
 	// STEP 2. Take two random elements in the array and swap them.
 	// STEP 3. Go back to step 1.
-	
-	//1. Complete the sort method using the Bogo sort algorithm. 
+
+	// 1. Complete the sort method using the Bogo sort algorithm.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
+		long i = 0, amount = 0;
+		while (!isSorted(array)) {
+			int a = rnd.nextInt(array.length);
+			int b = rnd.nextInt(array.length);
 
+			int temp = array[a];
+			array[a] = array[b];
+			array[b] = temp;
+
+			if (i == updateInterval) {
+				display.updateDisplay();
+				i = 0;
+				amount += updateInterval;
+
+				System.out.println("Amount of tries: " + amount);
+			}
+			i++;
+		}
+	}
+
+	boolean isSorted(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < array[i - 1]) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -1,33 +1,37 @@
 package _01_Searching_Algorithms;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+
 	@Test
 	public void testLinearSearch() {
-		//1. use the assertEquals method to test your linear search method.
+		String[] words = new String[] { "toaster", "plus", "bathtub" };
+		// 1. use the assertEquals method to test your linear search method.
+		assertEquals(0, _00_LinearSearch.linearSearch(words, "toaster"));
+		assertEquals(2, _00_LinearSearch.linearSearch(words, "bathtub"));
+		assertEquals(-1, _00_LinearSearch.linearSearch(words, "KCN"));
 	}
 
 	@Test
 	public void testBinarySearch() {
-		//2. use the assertEquals method to test your binary search method.
-		//   remember that the array must be sorted
+		// 2. use the assertEquals method to test your binary search method.
+		// remember that the array must be sorted
 	}
-	
+
 	@Test
 	public void testInterpolationSearch() {
-		//3. use the assertEquals method to test your interpolation search method.
-		//   remember that the array must be sorted and evenly distributed
+		// 3. use the assertEquals method to test your interpolation search method.
+		// remember that the array must be sorted and evenly distributed
 	}
-	
+
 	@Test
 	public void testExponentialSearch() {
-		//4. use the assertEquals method to test your exponential search method.
-		//   remember that the array must be sorted
+		// 4. use the assertEquals method to test your exponential search method.
+		// remember that the array must be sorted
 	}
 }
